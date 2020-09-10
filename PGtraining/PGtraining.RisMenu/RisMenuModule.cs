5fb2438812e1ddbde69670d09cabede94fe11e10
yoshikni <yoshikuni@ustec.co.jp>
2020-09-10 16:42:26 +0900

    Menuモジュールの追加

---
+++ PGtraining/PGtraining.RisMenu/RisMenuModule.cs
@@ -0,0 +1,20 @@
+﻿using PGtraining.RisMenu.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace PGtraining.RisMenu
+{
+    public class RisMenuModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
