43c5c6443dfa1b2c2035e1bb1b587fb80fc93421
yoshikni <yoshikuni@ustec.co.jp>
2020-09-10 17:55:53 +0900

    [add]ログイン画面を追加

--- PGtraining/PGtraining.RisMenu/RisMenuModule.cs
+++ PGtraining/PGtraining.RisMenu/RisMenuModule.cs
@@ -9,12 +9,15 @@ namespace PGtraining.RisMenu
     {
         public void OnInitialized(IContainerProvider containerProvider)
         {
-
+            // DIコンテナのPrismのフレームワークの画面管理
+            var regionManeger = containerProvider.Resolve<IRegionManager>();
+            regionManeger.RegisterViewWithRegion("ContentRegion", typeof(Views.Login));
         }
 
         public void RegisterTypes(IContainerRegistry containerRegistry)
         {
-
+            // DIコンテナにインスタンスを登録
+            containerRegistry.RegisterSingleton<Views.Login>();
         }
     }
 }
---
+++ PGtraining/PGtraining.RisMenu/Views/Login.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace PGtraining.RisMenu.Views
+{
+    /// <summary>
+    /// Interaction logic for Login
+    /// </summary>
+    public partial class Login : UserControl
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+    }
+}
