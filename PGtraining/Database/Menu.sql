0fb8d6386b09a4d67aae8bb8f5fb1bebc82e2fc1
yoshikni <yoshikuni@ustec.co.jp>
2020-09-10 20:48:31 +0900

    [add]DB追加

---
+++ PGtraining/Database/Menu.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Menu]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [OrderNo] NCHAR(8) NOT NULL, 
+    [MenuCode] NCHAR(8) NOT NULL, 
+    [MenuName] NVARCHAR(32) NOT NULL
+)
---
+++ PGtraining/Database/Orders.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Orders]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [OrderNo] NCHAR(8) NOT NULL, 
+    [StudyDate] NCHAR(8) NOT NULL, 
+    [ProcessingType] NCHAR(1) NOT NULL, 
+    [InspectionType] NVARCHAR(8) NOT NULL, 
+    [InspectionName] NVARCHAR(32) NOT NULL, 
+    [PatientId] NCHAR(10) NOT NULL, 
+    [PatientNameKanji] NVARCHAR(64) NOT NULL, 
+    [PatientNameKana] NVARCHAR(64) NOT NULL, 
+    [PatientBirth] DATE NOT NULL, 
+    [PatientSex] NCHAR(1) NOT NULL 
+)
