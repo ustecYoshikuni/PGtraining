1770761e9e349d3d2a0226a8d91d5a65d03c64b4
yoshikni <yoshikuni@ustec.co.jp>
2020-09-12 13:13:01 +0900

    [add]変換用のクラスを追加 [add]日付の文字列の変換関数を追加

---
+++ PGtraining/PGtraining.Lib/Change.cs
@@ -0,0 +1,16 @@
+﻿namespace PGtraining.Lib
+{
+    static public class Change
+    {
+        static public string YyyymmddToDateString(string date)
+        {
+            if (Lib.Check.IsAlphaNumericOnly(date.Substring(0, 7), true, 8))
+            {
+                var dateString = $"{date.Substring(0, 3)}/{date.Substring(4, 5)}{date.Substring(6, 7)}";
+                return dateString;
+            }
+
+            return "";
+        }
+    }
+}
