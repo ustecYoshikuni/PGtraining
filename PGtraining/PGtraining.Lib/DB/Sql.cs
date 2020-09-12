5b79875b9ced4163e9cf3bc6ef6063fc8804f67c
yoshikni <yoshikuni@ustec.co.jp>
2020-09-12 20:37:31 +0900

    [add]OrderNoの有無を確認する関数追加 [add]OrderのUpdate関数を追加

--- PGtraining/PGtraining.Lib/DB/Sql.cs
+++ PGtraining/PGtraining.Lib/DB/Sql.cs
@@ -7,7 +7,31 @@ namespace PGtraining.Lib.DB
 {
     public static class Sql
     {
-        public static bool InsertOrder(Import.Template order)
+        public static bool HasOrderNo(Order order)
+        {
+            var log = new Lib.Log.Log();
+            var result = false;
+
+            using (var connection = new SqlConnection())
+            using (var command = new SqlCommand())
+            {
+                try
+                {
+                    connection.ConnectionString = Setting.Setting.ConnectionString;
+                    connection.Open();
+
+                    var orders = connection.Get(order);
+                    result = (orders == null) ? false : true;
+                }
+                catch (Exception ex)
+                {
+                    log.Write(ex.ToString(), LevelEnum.ERRROR);
+                }
+            }
+            return result;
+        }
+
+        public static bool InsertOrder(Import.Template template)
         {
             var log = new Lib.Log.Log();
             var result = false;
@@ -22,7 +46,41 @@ namespace PGtraining.Lib.DB
 
                     using (var tran = connection.BeginTransaction())
                     {
-                        connection.Insert(order.Order, statement => statement.AttachToTransaction(tran));
+                        connection.Insert(template.Order, statement => statement.AttachToTransaction(tran));
+
+                        foreach (var menu in template.MenuList)
+                        {
+                            connection.Insert(menu, statement => statement.AttachToTransaction(tran));
+                        }
+                        tran.Commit();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    log.Write(ex.ToString(), LevelEnum.ERRROR);
+                }
+            }
+            return result;
+        }
+
+        public static bool UpdateOrder(Import.Template order)
+        {
+            var log = new Lib.Log.Log();
+            var result = false;
+
+            using (var connection = new SqlConnection())
+            using (var command = new SqlCommand())
+            {
+                try
+                {
+                    connection.ConnectionString = Setting.Setting.ConnectionString;
+                    connection.Open();
+
+                    using (var tran = connection.BeginTransaction())
+                    {
+                        connection.Update(order.Order, statement => statement.AttachToTransaction(tran));
+
+                        connection.BulkDelete<Menu>(statement => statement.Where($"{nameof(Menu.OrderNo):C}={order.Order.OrderNo}").AttachToTransaction(tran));
 
                         foreach (var menu in order.MenuList)
                         {
