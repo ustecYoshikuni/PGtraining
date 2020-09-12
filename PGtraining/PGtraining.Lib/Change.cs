551d52c0ac602fae82f68ac42d4a85b7e5e97a0f
yoshikni <yoshikuni@ustec.co.jp>
2020-09-12 13:59:44 +0900

    [fix]substringの使い方ミスを修正

--- PGtraining/PGtraining.Lib/Change.cs
+++ PGtraining/PGtraining.Lib/Change.cs
@@ -4,7 +4,7 @@
     {
         static public string YyyymmddToDateString(string date)
         {
-            if (Lib.Check.IsAlphaNumericOnly(date.Substring(0, 8), true, 8))
+            if (Lib.Check.IsAlphaNumericOnly(date.Substring(0, 8), true, 1, 8))
             {
                 var dateString = $"{date.Substring(0, 4)}/{date.Substring(4, 2)}/{date.Substring(6, 2)}";
                 return dateString;
