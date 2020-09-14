7a9b71630f963a615db896118bbf1444d77849a4
yoshikni <yoshikuni@ustec.co.jp>
2020-09-14 12:04:38 +0900

    [fix]仕様との不一致を修正

--- PGtraining/Database/Menu.sql
+++ PGtraining/Database/Menu.sql
@@ -2,6 +2,6 @@
 (
 	[Id] INT NOT NULL PRIMARY KEY, 
     [OrderNo] NCHAR(8) NOT NULL, 
-    [MenuCode] NCHAR(8) NOT NULL, 
+    [MenuCode] VARCHAR(8) NOT NULL, 
     [MenuName] NVARCHAR(32) NOT NULL
 )
