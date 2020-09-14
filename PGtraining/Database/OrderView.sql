2ed91a0f631d11e0c59f60862fb421b6280eb46a
yoshikni <yoshikuni@ustec.co.jp>
2020-09-14 12:06:11 +0900

    [add]撮影項目コードと撮影項目名称を追加したView

---
+++ PGtraining/Database/OrderView.sql
@@ -0,0 +1,44 @@
+﻿CREATE VIEW [dbo].[OrderView]
+	AS
+    Select
+ O.[OrderNo]
+,O.[StudyDate]
+,O.[ProcessingType]
+,O.[InspectionType]
+,O.[InspectionName]
+,O.[PatientId]
+,O.[PatientNameKanji]
+,O.[PatientNameKana]
+,O.[PatientBirth]
+,O.[PatientSex]
+,M.codes AS MenuCodes
+,M.names As MenuNames
+from Orders As O
+ INNER JOIN 
+  (SELECT Main.OrderNo,
+   LEFT(Main.names,Len(Main.names)-1) As "names"
+ , LEFT(Main.codes,Len(Main.codes)-1) As "codes"
+FROM
+    (
+
+        SELECT DISTINCT M.OrderNo, 
+            (
+                SELECT M1.MenuName + ',' AS [text()]
+                FROM Menu as M1
+                WHERE M1.OrderNo = M.OrderNo
+                ORDER BY M1.OrderNo
+                FOR XML PATH ('')
+            ) names,
+            (
+                SELECT M1.MenuCode + ',' AS [text()]
+                FROM Menu as M1
+                WHERE M1.OrderNo = M.OrderNo
+                ORDER BY M1.OrderNo
+                FOR XML PATH ('')
+            ) codes
+        FROM Menu  as  M
+    ) as Main)
+ as M 
+	ON O.OrderNo = M.OrderNo
+    
+		
