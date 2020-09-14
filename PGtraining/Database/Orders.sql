94a9b7e5fe3768d15e6d04edc2642b7f3941f561
yoshikni <yoshikuni@ustec.co.jp>
2020-09-14 12:05:33 +0900

    [update]日付データを文字列に修正：文字列の方があつかいやすいと感じたため

--- PGtraining/Database/Orders.sql
+++ PGtraining/Database/Orders.sql
@@ -9,6 +9,6 @@
     [PatientId] NCHAR(10) NOT NULL, 
     [PatientNameKanji] NVARCHAR(64) NOT NULL, 
     [PatientNameKana] NVARCHAR(64) NOT NULL, 
-    [PatientBirth] DATE NOT NULL, 
+    [PatientBirth] NCHAR(8) NOT NULL, 
     [PatientSex] NCHAR(1) NOT NULL 
 )
